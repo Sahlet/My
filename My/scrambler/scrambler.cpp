07124caa6bb9217997f80734cab2ba05d487e253
Unknown <sahlet@yandex.ru>
2019-05-29 14:34:48 +0300

    changing console interface and key concept

--- My/scrambler/scrambler.cpp
+++ My/scrambler/scrambler.cpp
@@ -8,7 +8,7 @@ namespace
 {
 	auto const ShortDescription = "This program performs encrypting and decrypting of sources";
 	auto const DefaultPathValue = "./";
-	auto const EncriptionAlgorithmName = "...";
+	unsigned short const DefaultKeyLength = 32;
 
 	struct ProgramOption
 	{
@@ -27,17 +27,20 @@ namespace
 		Encrypt{"encryp", 'e'},
 		Decrypt{"decrypt", 'd'},
 		Key{"key", 'k'},
-		Path{"path", 'p'};
+		Path{"path", 'p'},
+		GenKey{"gen-key", 'g'};
 
 	std::string getUsageHelp()
 	{
 		auto usage = "usage: scrambler ";
 		auto usageLen = strlen(usage);
 		std::stringstream sstream;
-		sstream << usage << "[--" << Help.m_cFullName << "]" << "[--" << Encrypt.m_cFullName << "]"
-			<< "[--" << Decrypt.m_cFullName << "]" << std::endl
-			<< std::string(usageLen, ' ') << "[--" << Key.m_cFullName << "=<" << Key.m_cFullName << ">]"
-			<< "[--" << Path.m_cFullName << "=<" << Path.m_cFullName << ">]" << std::endl;
+		sstream << usage << "[--" << Help.m_cFullName << "]" << " [--" << Encrypt.m_cFullName << "]"
+			<< " [--" << Decrypt.m_cFullName << "]" << std::endl
+			<< std::string(std::max(0, int(usageLen) - 1), ' ')
+			<< " [--" << Key.m_cFullName << "=<" << Key.m_cFullName << ">]"
+			<< " [--" << Path.m_cFullName << "=<" << Path.m_cFullName << ">]"
+			<< " [--" << GenKey.m_cFullName << " <key length>]" << std::endl;
 		return sstream.str();
 	}
 
@@ -55,9 +58,8 @@ namespace
 			<< " and converts it to sources files into folder" << std::endl
 			<< " <path>/src_encrypt/src_YYYY-MM-DD-HH-MM-SS/" << std::endl;
 
-		sstream << "-if you need more security - use key option;" << std::endl
-			<< " encryption algorithm is " << EncriptionAlgorithmName << std::endl
-			<< " so key is asymmetric for encrypting and decrypting" << std::endl;
+		sstream << "-if you need more security - use '" << Key.m_cFullName << "' option;" << std::endl
+			<< " to generate new keys use '" << GenKey.m_cFullName << "' option" << std::endl;
 
 		return sstream.str();
 	}
@@ -67,13 +69,14 @@ int main(int argc, char* argv[])
 {
 	try
 	{
-		boostPO::options_description desc{"Options"};
+		boostPO::options_description desc("Options");
 		desc.add_options()
 		((Help.m_cFullName + std::string(",") + Help.m_cShortName).c_str(), "Help screen")
 		((Encrypt.m_cFullName + std::string(",") + Encrypt.m_cShortName).c_str(), "Indicates that sources should be encrypted")
 		((Decrypt.m_cFullName + std::string(",") + Decrypt.m_cShortName).c_str(), "Indicates that the encrypted file should be decrypted back to the sources")
-		((Key.m_cFullName + std::string(",") + Key.m_cShortName).c_str(), boostPO::value<std::string>(), "Key for encrypting or decrypting")
-		((Path.m_cFullName + std::string(",") + Path.m_cShortName).c_str(), boostPO::value<std::string>()->default_value(DefaultPathValue), "Path where to execute encrypting or decrypting");
+		((Key.m_cFullName + std::string(",") + Key.m_cShortName).c_str(), boostPO::value<std::string>(), "Symmetric key for encrypting or decrypting")
+		((Path.m_cFullName + std::string(",") + Path.m_cShortName).c_str(), boostPO::value<std::string>()->default_value(DefaultPathValue), "Path where to execute encrypting or decrypting")
+		((GenKey.m_cFullName + std::string(",") + GenKey.m_cShortName).c_str(), boostPO::value<unsigned short>()->default_value(DefaultKeyLength), "Generates new keys (arg is length of key)");
 
 		boostPO::variables_map vm;
 		boostPO::store(boostPO::parse_command_line(argc, argv, desc), vm);
