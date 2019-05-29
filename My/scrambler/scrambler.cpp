efe62957dfdd5069556a2922ee31439b0aea6f42
Unknown <sahlet@yandex.ru>
2019-05-29 20:22:01 +0300

    Revert "gen-key with constant length"
    This reverts commit 61229cb24080df4cb2d77695b21dbeb3358b0e87.

--- My/scrambler/scrambler.cpp
+++ My/scrambler/scrambler.cpp
@@ -8,7 +8,7 @@ namespace
 {
 	auto const ShortDescription = "This program performs encrypting and decrypting of sources";
 	auto const DefaultPathValue = "./";
-	auto const KeyLength = 32;
+	unsigned short const DefaultKeyLength = 32;
 
 	struct ProgramOption
 	{
@@ -26,26 +26,21 @@ namespace
 		Help{"help", 'h'},
 		Encrypt{"encryp", 'e'},
 		Decrypt{"decrypt", 'd'},
-		GenKey{"gen-key", 'g'},
 		Key{"key", 'k'},
-		Path{"path", 'p'};
+		Path{"path", 'p'},
+		GenKey{"gen-key", 'g'};
 
 	std::string getUsageHelp()
 	{
-		auto usage = "usage: scrambler";
+		auto usage = "usage: scrambler ";
 		auto usageLen = strlen(usage);
 		std::stringstream sstream;
-		sstream << usage
-			<< " [--" << Help.m_cFullName << "]"
-			<< " [--" << Encrypt.m_cFullName << "]"
-			<< " [--" << Decrypt.m_cFullName << "]"
-			<< " [--" << GenKey.m_cFullName << "]"
-			<< std::endl
-			<< std::string(usageLen, ' ')
+		sstream << usage << "[--" << Help.m_cFullName << "]" << " [--" << Encrypt.m_cFullName << "]"
+			<< " [--" << Decrypt.m_cFullName << "]" << std::endl
+			<< std::string(std::max(0, int(usageLen) - 1), ' ')
 			<< " [--" << Key.m_cFullName << "=<" << Key.m_cFullName << ">]"
 			<< " [--" << Path.m_cFullName << "=<" << Path.m_cFullName << ">]"
-			<< std::endl;
-
+			<< " [--" << GenKey.m_cFullName << " <key length>]" << std::endl;
 		return sstream.str();
 	}
 
@@ -79,9 +74,9 @@ int main(int argc, char* argv[])
 		((Help.m_cFullName + std::string(",") + Help.m_cShortName).c_str(), "Help screen")
 		((Encrypt.m_cFullName + std::string(",") + Encrypt.m_cShortName).c_str(), "Indicates that sources should be encrypted")
 		((Decrypt.m_cFullName + std::string(",") + Decrypt.m_cShortName).c_str(), "Indicates that the encrypted file should be decrypted back to the sources")
-		((GenKey.m_cFullName + std::string(",") + GenKey.m_cShortName).c_str(), "Generates new key")
 		((Key.m_cFullName + std::string(",") + Key.m_cShortName).c_str(), boostPO::value<std::string>(), "Symmetric key for encrypting or decrypting")
-		((Path.m_cFullName + std::string(",") + Path.m_cShortName).c_str(), boostPO::value<std::string>()->default_value(DefaultPathValue), "Path where to execute encrypting or decrypting");
+		((Path.m_cFullName + std::string(",") + Path.m_cShortName).c_str(), boostPO::value<std::string>()->default_value(DefaultPathValue), "Path where to execute encrypting or decrypting")
+		((GenKey.m_cFullName + std::string(",") + GenKey.m_cShortName).c_str(), boostPO::value<unsigned short>()->default_value(DefaultKeyLength), "Generates new keys (arg is length of key)");
 
 		boostPO::variables_map vm;
 		boostPO::store(boostPO::parse_command_line(argc, argv, desc), vm);
