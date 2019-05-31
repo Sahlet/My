12ccc647cdc60115029b130b151bd191c3e088b3
Unknown <sahlet@yandex.ru>
2019-05-31 16:15:43 +0300

    link to needed libs

--- My/scrambler/scramblerBuild.sh
+++ My/scrambler/scramblerBuild.sh
@@ -1,1 +1,1 @@
-g++ -std=c++11 scrambler.cpp -o scrambler -L /usr/lib/ -lboost_program_options
+g++ -std=c++11 scrambler.cpp -o scrambler -I ../../ -L /usr/lib/ -lboost_program_options -lboost_system -lboost_filesystem -lpthread
